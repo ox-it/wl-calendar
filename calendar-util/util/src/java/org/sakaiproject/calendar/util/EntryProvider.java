bfb7ad2e0fd0d5e7420e695bdc79cc6623bfddbc
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2010-02-22 10:15:30 +0000

    Add Sakai license to files.

--- calendar-util/util/src/java/org/sakaiproject/calendar/util/EntryProvider.java
+++ calendar-util/util/src/java/org/sakaiproject/calendar/util/EntryProvider.java
@@ -1,3 +1,23 @@
+/**********************************************************************************
+ * $URL$
+ * $Id$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2003, 2004, 2005, 2006, 2008 The Sakai Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
 package org.sakaiproject.calendar.util;
 
 import org.sakaiproject.calendar.api.Calendar;
@@ -8,10 +28,6 @@ import org.sakaiproject.exception.PermissionException;
 import org.sakaiproject.site.cover.SiteService;
 import org.sakaiproject.util.MergedListEntryProviderBase;
 
-/**
- * Provides a list of merged calendars by iterating through all
- * available calendars.
- */
 public class EntryProvider extends MergedListEntryProviderBase
 {
 	private CalendarService calendarService;
