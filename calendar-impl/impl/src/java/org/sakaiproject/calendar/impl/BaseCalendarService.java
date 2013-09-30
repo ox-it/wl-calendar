fcb3371da67f5ddf9c6e4b7749a33ef70bd01483
Ben Holmes <ben.holmes@it.ox.ac.uk>
2013-09-30 14:17:34 +0100

    Dropping the test altogether since numEvents will never be < -1.

--- calendar-impl/impl/src/java/org/sakaiproject/calendar/impl/BaseCalendarService.java
+++ calendar-impl/impl/src/java/org/sakaiproject/calendar/impl/BaseCalendarService.java
@@ -6927,12 +6927,11 @@ public abstract class BaseCalendarService implements CalendarService, StorageUse
 		ical.getComponents().add(registry.getTimeZone(tzId.getValue()).getVTimeZone());
 		
 		CalendarOutputter icalOut = new CalendarOutputter();
-		int numEvents = generateICal(ical, calRefs);
+		generateICal(ical, calRefs);
 			
 		try 
 		{
-			if ( numEvents >= 0 )
-				icalOut.output( ical, os );
+			icalOut.output( ical, os );
 		}
 		catch (Exception e)
 		{
