0823d3b9925b92cd5830e1497d6aeb6e0f52bf2c
Neelam Bhandari <neelam.bhandari@it.ox.ac.uk>
2016-06-15 11:40:57 +0100

    WL-4274 Modified calendar entity to support calendar component in Lessons.
    Exposed method 'getCalendarReferences' in CalendarService to get
    references from all calendars for a site.
    
    Added attributes 'description' and 'eventImage' in the EventSummary class
    which will be used in the json returned by CalendarEventEntityProvider.
    
    Created map for event type and image in the CalendarUtil class which can be
    accessed using method 'getEventImageMap'.
    
    CalendarEventEntityProvider's 'getCalendarEventsForSite' will also return
    events from all the merged calendars for a site depending on the parameters
    in the request.If 'merged' parameter is set method will return events from
    all merged calendars else events from the current site will be displayed.
    Pulled latest code for class 'CalendarEventEntityProvider' from Sakai trunk

--- calendar-tool/tool/src/java/org/sakaiproject/calendar/entityproviders/CalendarEventSummary.java
+++ calendar-tool/tool/src/java/org/sakaiproject/calendar/entityproviders/CalendarEventSummary.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import org.sakaiproject.calendar.api.CalendarEvent;
 import org.sakaiproject.calendar.api.RecurrenceRule;
 import org.sakaiproject.time.api.Time;
+import org.sakaiproject.util.CalendarUtil;
 
 @Data
 public class CalendarEventSummary {
@@ -16,7 +17,20 @@ public class CalendarEventSummary {
 	private String creator;
 	private Time firstTime;
 	private long duration;
+	private String description;
 	private RecurrenceRule recurrenceRule;
+
+	/**
+	 * This field will only be set if the event is an assignment and can be used to reconstrut the deepLink
+	 */
+	private String assignmentId;
+
+	/**
+	 * Set externally after object creation, signals the site the event came from (not part of CalendarEvent)
+	 */
+	private String siteId;
+	//icon used for specific eventType
+	private String eventImage;
 
 	public CalendarEventSummary() {
 	}
@@ -31,6 +45,8 @@ public class CalendarEventSummary {
 		firstTime = event.getRange().firstTime();
 		duration = event.getRange().duration();
 		recurrenceRule = event.getRecurrenceRule();
+		description = event.getDescriptionFormatted();
+		assignmentId = event.getField(CalendarUtil.NEW_ASSIGNMENT_DUEDATE_CALENDAR_ASSIGNMENT_ID);
 	}
 
 }
